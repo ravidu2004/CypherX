ed4a9d650560f2e2364574ac4ab22116cc39c22d
ravidu2004 <sandeepavidu65@gmail.com>
2025-02-12 17:49:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER-BOT:~u3IxzKCB#7LqM1HlbSO4dOMc4zymnq7DPqwLAzLQgHsOXrw1Ohwk' 
 //Enter your Xploader session id here; must start with XPLOADER-BOT:~
 
 //=================================================//
@@ -23,7 +23,7 @@ global.botname = process.env.BOT_NAME || 'CypherX'
 //Your desired bot name
 
 //=================================================//
-global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
+global.ownernumber = process.env.OWNER_NUMBER || '+94705215918' 
 //Type your main number here
 
 //=================================================//
